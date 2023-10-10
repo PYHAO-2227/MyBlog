81532424696252c49acac0ddbaf8e296737ff09d
PYHAO <pyhao@umich.edu>
2023-10-09 22:44:52 -0400

    Add: Exceptions handler + DTO upgrade + Dependencys

--- src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
@@ -1,10 +1,15 @@
 package com.springboot.blog.springbootblogrestapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBlogRestApiApplication {
+
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
